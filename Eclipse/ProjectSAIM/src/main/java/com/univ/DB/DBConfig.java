0d3b6747a3b2ab9134822e471318952d9591c85a
Vivek30092 <golu423806@gmail.com>
2025-05-15 16:59:19 +0530

    Thur,15 May, 2025, 4:59PM

---
+++ Eclipse/ProjectSAIM/src/main/java/com/univ/DB/DBConfig.java
@@ -0,0 +1,8 @@
+package com.univ.DB;
+
+public interface DBConfig {
+	String DRIVER = "com.mysql.cj.jdbc.Driver";
+	String URL = "jdbc:mysql://localhost:3306/SAIT1";
+	String UNM = "root";
+	String PW = "";
+}
