c73272cb18801ef383eebb5b8c6a39c15fafc08f
Vivek30092 <golu423806@gmail.com>
2025-05-22 00:52:56 +0530

    Thur, 22 May, 2025, 12:52 AM

---
+++ ADA/QuickSort.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class QuickSort {
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int p = partition(arr, low, high);
+            quickSort(arr, low, p - 1);
+            quickSort(arr, p + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // swap arr[i + 1] and pivot
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int val : arr)
+            System.out.print(val + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Name.info();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        quickSort(arr, 0, n - 1);
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
