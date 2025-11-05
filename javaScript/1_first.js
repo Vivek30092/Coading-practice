8ee4e94a2de179a65a7a7905265144882b7a27a6
Vivek30092 <golu423806@gmail.com>
2025-11-05 18:36:23 +0530

    Wed, 5 Nov 2025, 06:36 PM

---
+++ javaScript/1_first.js
@@ -0,0 +1,23 @@
+console.log("Hello world");
+console.log("vivek kumar choudhary");
+console.log("first file ")
+
+// variables in JS
+
+fullName = "John"
+console.log(fullName)
+
+age = 23
+console.log(age)
+
+price = 99.99
+x = null 
+y = undefined(y)
+console.log(x)
+console.log(y)
+
+
+let name = "vivek"
+let age = 23;
+console.log(name)
+
---
+++ javaScript/2_variable.js
@@ -0,0 +1,24 @@
+// var age = 21   //not in used and we will not used it again
+// age = 32
+// console.log(age)
+
+
+
+let age = 34
+age = 23
+console.log(age)
+
+let unknown ;
+console.log(unknown)
+
+const student = {
+    fullName : "tony",
+    age : 48,
+    cgpa : 9.9,
+    ifPass : false,
+};
+
+student["age"] +=1
+console.log(student)
+console.log(student["fullName"])
+console.log(student.age)
