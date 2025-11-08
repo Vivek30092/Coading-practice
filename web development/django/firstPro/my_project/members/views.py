5539d3b3d068acee6afdce7de2ec8f93f1b307b3
Vivek30092 <golu423806@gmail.com>
2025-11-08 22:53:07 +0530

    Sat, 8 Nov, 2025, 10:52 PM

--- web development/django/firstPro/my_project/members/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
---
+++ web_development/Django/edX/lecture3edX/hello/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,"hello/index.html")
+
+
+def vivek(request):
+    return HttpResponse("hello vivek!")
+
+def deva(request):
+    return HttpResponse("Hello, deva!")
+
+def greet(request,name):
+    return render(request,"hello/greet.html",{
+        "name" : name.title()
+    })
