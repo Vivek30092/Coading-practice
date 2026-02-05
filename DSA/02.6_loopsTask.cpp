5703115e74ccd2c9b4be0c2f0d6205ccec652a26
Vivek30092 <golu423806@gmail.com>
2026-02-05 19:36:56 +0530

    Thur, 05 Feb, 2026, 07:36 PM

--- DSA/02.6_loopsTask.cpp
+++ DSA/02.6_loopsTask.cpp
@@ -1,1 +1,18 @@
 // check if a number is prime or not
+#include<iostream>
+using namespace std;
+int main(){
+    // check weather given number is prime or not
+    int num;
+    cout<<"enter number to check isPrime : ";
+    cin>>num;
+    for(int i = 2;i<=(num-1);i++){
+        if(num%i!=0){
+            cout<<"prime number";
+            break;
+        }else{
+            cout<<"not prime";
+        }
+    }
+    // prime number between given number
+}
