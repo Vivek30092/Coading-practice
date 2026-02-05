2b5551ce95a2c043414ae81d10f9588392436c71
Vivek30092 <golu423806@gmail.com>
2026-02-06 00:57:47 +0530

    Fri, 06 Feb, 2026, 12:57 AM

--- DSA/03.1_patterns.cpp
+++ DSA/03.1_patterns.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 using namespace std;
 int main(){
+    // for number
+    cout<<"pattern for digits...\n";
     int num;
     num = 3;
-    int number = 1;
+    int digit = 1;
     for(int i = 0;i<num;i++){
         for(int j = 0;j<num;j++){
-            cout<<number<<" ";
-            number +=1;
+            cout<<digit<<" ";
+            digit +=1;
+        }
+        cout<<endl;
+    }
+
+    cout<<"\n\npattern for char... \n";
+
+    // for char
+    char ch = 'A';
+    for(int i = 0;i<num;i++){
+        for(int j = 0; j<num; j++){
+            cout<<ch<<" ";
+            ch +=1;
         }
         cout<<endl;
     }
