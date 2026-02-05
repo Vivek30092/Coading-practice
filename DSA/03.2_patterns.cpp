4e1e12f9c37ed2e4b36b3362402ef62338a400b0
Vivek30092 <golu423806@gmail.com>
2026-02-06 01:00:48 +0530

    Fri, 06 Feb, 2026, 01:00 AM

---
+++ DSA/03.2_patterns.cpp
@@ -0,0 +1,15 @@
+// triangle pattern
+/*
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int num = 4;
+    for(int i = 0; i<num; i++){
+        for(int j = 0;j<=i;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
