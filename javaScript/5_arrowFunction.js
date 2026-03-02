c82dd21a41d5a9ef32e1b14b869b030039e81377
Vivek30092 <golu423806@gmail.com>
2026-03-02 23:23:54 +0530

    Mon, 02 March, 2026, 11:23 PM

---
+++ javaScript/5_arrowFunction.js
@@ -0,0 +1,14 @@
+// sum function 
+function sum(a,b){
+    return a+b;
+}
+
+// multiplication function
+function mul(a,b){
+    return a*b;
+}
+
+// arrowFunction
+const arrowSum = (a,b)=>{
+    console.log(a+b);
+}
--- javaScript/5_functions.js
+++ javaScript/5_functions.js
@@ -3,3 +3,16 @@ function greet(name){      // here name is 'parameter'
 }
 
 greet("Harry");         // Argument
+
+function sum(num1,num2){
+    // console.log("sum of two number is ",num1+num2);
+    return num1+num2;
+}
+let val = sum(5,0);
+console.log(val);
+
+
+// compact function
+
+
+
