42565803c44ba513ce40b05fbbffcd2c83554eab
Vivek30092 <golu423806@gmail.com>
2026-03-03 23:39:44 +0530

    Tues, 03 March, 2026, 11:39 PM

--- DSA/06.1_operPrecedence.cpp
+++ DSA/06.1_operPrecedence.cpp
@@ -10,3 +10,30 @@ bitwise operator                fourth
 ||                              seventh             [same as above]
 =(assignment operator)          eighth              [R to left]
 */
+
+
+// scope
+/*
+1. Local scope
+2. global scope
+*/
+
+
+// data type modifiers : changes meaning of data types
+/*
+1. long(>=4B) : longint, longdouble
+2. short()
+3. long long
+4. singed
+5. unsigned
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<sizeof(int)<<endl;
+    cout<<sizeof(long int)<<endl;
+    cout<<sizeof(short int)<<endl;
+    cout<<sizeof(long long int)<<endl;
+    return 0;
+}
