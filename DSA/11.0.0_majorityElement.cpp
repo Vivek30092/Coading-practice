b679d47c36a441f47a1c90acfda60c564334e34d
Vivek30092 <golu423806@gmail.com>
2026-03-20 21:08:34 +0530

    Fri, 20 March, 2026, 09:08 PM

---
+++ DSA/11.0.0_majorityElement.cpp
@@ -0,0 +1,21 @@
+// return majority element of array[element appears more than n/2 times]
+// using bruteForce approach
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> vec = {2,2,1,1,1,2,2};
+    int size = vec.size();
+    for(int val : vec){
+        int count = 0;
+        for(int el : vec){
+            if(val == el){
+                count++;
+            }
+        }
+        if(count>size/2){
+            return val;
+        }        
+    }
+    return -1;
+};
---
+++ DSA/11.0.1_majorityElement.cpp
@@ -0,0 +1,8 @@
+// return majority element of array[element appears more than n/2 times]
+// using optimize approach using sorting
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    
+};
