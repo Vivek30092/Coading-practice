726d4ea2b16f21fe5cb3591293ed640bd72f079f
Vivek30092 <golu423806@gmail.com>
2026-03-21 23:18:37 +0530

    Sat, 21 March, 2026, 11:18 PM

--- DSA/11.0.1_majorityElement.cpp
+++ DSA/11.0.1_majorityElement.cpp
@@ -1,8 +1,32 @@
 // return majority element of array[element appears more than n/2 times]
 // using optimize approach using sorting
+// time complexity of sorted array : O(nlogn)
 #include<iostream>
 #include<vector>
 using namespace std;
 int main(){
+    class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        
+
+        // sort
+        sort(nums.begin(),nums.end());
+
+        // freq count
+        int freq = 1, ans = nums[0];
+        for(int i = 1; i<n ;i++){
+            if(nums[i]==nums[i-1]){
+                freq++;
+            }
+            else{
+                freq = 1;
+                ans = nums[i];
+            }
+        }
+        return ans;
+    }
+};
     
 };
