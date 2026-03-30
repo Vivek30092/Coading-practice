8b08ee831afc57ec881440d3ab8d9fd60f0db289
Vivek30092 <golu423806@gmail.com>
2026-03-30 23:28:29 +0530

    Mon, 30 March, 2026, 11:28 PM

---
+++ DSA/14.0.1_containerProblem.cpp
@@ -0,0 +1,9 @@
+// optimal approach : 2 pointer approach
+// return max amount of water container can store
+// problem 11 on leetCode
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    return 0;
+}
