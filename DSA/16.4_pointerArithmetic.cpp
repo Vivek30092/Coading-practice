e30a7026639fefff7c0db8517a166e9464a45821
Vivek30092 <golu423806@gmail.com>
2026-04-07 23:32:37 +0530

    Tue, 07 Apr, 2026, 11:32 PM

--- DSA/16.4_pointerArithmetic.cpp
+++ DSA/16.4_pointerArithmetic.cpp
@@ -1,5 +1,16 @@
+
 #include<iostream>
 using namespace std;
 int main(){
+    // Increment(++)/Decrement(--) : size incr. or decr. by size of pointer based on datatype
+    int a = 10;
+    int *ptr = &a;
+    cout<<ptr<<endl;
+    ptr++;      //size of int is 4
+    cout<<ptr<<endl;
+
+    // add/subtract number
+    cout<<ptr+1<<endl;      //add 4bite of storage space
+    cout<<ptr-1<<endl;      //1int = 4B
     return 0;
 }
