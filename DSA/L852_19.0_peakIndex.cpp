eb2652ee0906f882cabc7d604bd1aec77dd46969
Vivek30092 <golu423806@gmail.com>
2026-04-12 00:05:31 +0530

    Sun, 12 Apr, 2026, 12:05 AM

--- DSA/L852_19.0_peakIndex.cpp
+++ DSA/L852_19.0_peakIndex.cpp
@@ -1,2 +1,26 @@
 // Prob. 852. : Peak Index in Mountain array problem
-// Prob. 852 on leetcode
+// Prob. 852 on leetCode
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int peakIndexInMountainArray(vector<int> &nums){
+    int st = 0,end = nums.size()-1;
+    while(st<=end){
+        int mid = st + (st+end)/2;
+        if(nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1]){
+            return mid;
+        }
+        if(nums[mid-1] < nums[mid]){    //right search
+            st = mid + 1;
+        }else{                          //left search
+            end = mid - 1;
+        }
+        return -1;
+        } 
+}
+int main(){
+    vector<int> nums = {0,10,5,2};
+    cout<<peakIndexInMountainArray(nums);
+    return 0;
+}
