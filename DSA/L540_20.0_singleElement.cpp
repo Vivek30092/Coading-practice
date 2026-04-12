dee8d11e43c8d98c6bac606b8bf40c312170ac82
Vivek30092 <golu423806@gmail.com>
2026-04-12 19:57:35 +0530

    Sun, 12 Apr, 2026, 07:57 PM

--- DSA/L540_20.0_singleElement.cpp
+++ DSA/L540_20.0_singleElement.cpp
@@ -3,35 +3,37 @@
 #include<vector>
 using namespace std; 
 int singleNonDuplicate(vector<int>& nums) {
-        int st = 0, end = nums.size()-1;
-        if(nums.size()==1) return nums[0];
-        while(st<=end){
-            int mid = st + (end-st)/2;
-            if(mid == 0 && nums[0]!=nums[1]) return nums[mid];
-            if(mid == nums.size()-1 && nums[nums.size()-1] != nums[nums.size()-2]) return nums[mid];
+    int n = nums.size();
 
-            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
-                return mid;
-            }
-            if(mid%2==0){       //even number at both side of mid
-                if(nums[mid-1] == nums[mid]){
-                    end = mid-1;
-                }else{
-                    st = mid+1;
-                }
-            }else{              // odd number at both side of mid
-                if(nums[mid-1] == nums[mid]){
-                    st = mid+1;
-                }else{
-                    end = mid-1;
-                }
-            }
-        return mid;
+    if(n == 1) return nums[0];
+
+    int st = 0, end = n-1;
+    while(st<=end){
+        int mid = st + (end-st)/2;
+        if(mid == 0 && nums[0]!=nums[1]) return nums[mid];
+        if(mid == n-1 && nums[n-1] != nums[n-2]) return nums[mid];
+
+        if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+            return mid;
         }
-        // return -1;
+        if(mid%2 == 0){       //even number at both side of mid
+            if(nums[mid-1] == nums[mid]){
+                end = mid-1;
+            }else{
+                st = mid+1;
+            }
+        }else{              // odd number at both side of mid
+            if(nums[mid-1] == nums[mid]){
+                st = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
     }
+    return -1;
+}
 int main(){
-    vector<int> arr = {};
+    vector<int> arr = {1,1,2,3,3,4,4,8,8};
     cout<<singleNonDuplicate(arr);
     return 0;
 }
